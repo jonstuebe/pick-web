82fe85687c9bfbdea19783e1358af6c8dbf6766a
Jon Stuebe <jstuebe@gmail.com>
2021-06-18 16:23:16 -0700

    fixed title warning on _document and updated styles on headers

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,6 @@ export default class AppDocument extends Document {
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍔</text></svg>"
           />
 
-          <title>Pick</title>
           <meta name="description" content="a better way to pick dinner" />
 
           <meta property="og:url" content="https://getpick.net" />
